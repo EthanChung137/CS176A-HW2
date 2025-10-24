e9055db858e2d482f5b3b8ca6516af597e838ab7
Ethan Chung <ethanchung@ucsb.edu>
2025-10-23 18:15:11 -0700

    Readded buffer[n] = \0

--- client_c_tcp.c
+++ client_c_tcp.c
@@ -64,6 +64,17 @@ int main(int argc, char *argv[])
             break;
         }
 
+        buffer[n] = '\0';
+
+        //Removing any '\r' characters due to its ability to "overwrite" the "From server: " message
+        // int i, k = 0;
+        // for (i = 0; buffer[i] != '\0'; i++) {
+        //     if (buffer[i] != '\r' && buffer[i] != '\n') {
+        //         buffer[k++] = buffer[i];
+        //     }
+        // }
+        // buffer[k] = '\0';
+
         printf("From server: %s\n", buffer);
         fflush(stdout);
 
